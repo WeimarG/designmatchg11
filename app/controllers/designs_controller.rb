a24f93efd34581ea1745f6eb9199c64154b72b4e
WeimarG <70210268+WeimarG@users.noreply.github.com>
2020-11-17 23:09:10 -0500

    SQS AWS

--- app/controllers/designs_controller.rb
+++ app/controllers/designs_controller.rb
@@ -71,7 +71,7 @@ class DesignsController < ApplicationController
 
   def send_message(message)
     sqs = Aws::SQS::Client.new(region: 'us-east-2')
-    queue_url = "https://sqs.us-east-2.amazonaws.com/296466592965/MessagesQueue"
+    queue_url = "https://sqs.us-east-2.amazonaws.com/994617978174/designsqs.fifo"
     sqs.send_message(queue_url: queue_url, message_body: message)
   end
 end
